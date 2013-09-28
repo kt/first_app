aee7c538ed21466721c3a277e5a50eff6cbca6d8
Gary Phillips <github@rolmo.com>
2013-09-28 23:22:04 +0200

    Experimenting with routes directives

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 FirstApp::Application.routes.draw do
-  get "static_pages/home"
+  get 'static_pages/home' => 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/wibble'
+  get '/' => 'static_pages#wibble'
   root 'static_pages#wibble'
 
   # The priority is based upon order of creation: first created -> highest priority.
