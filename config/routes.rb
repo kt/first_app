7de24fd6997d846cf2ea10f4e9ff5941516bbe9d
Gary Phillips <github@rolmo.com>
2013-10-02 20:45:45 +0200

    Adding user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FirstApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', to: 'users#new', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#conact', via: 'get'
+  
   get 'static_pages/home' => 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/wibble'
